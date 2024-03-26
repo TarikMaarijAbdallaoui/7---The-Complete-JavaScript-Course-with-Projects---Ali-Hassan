cbfa151152232af9655707a299bdd0c01c98557e
tarikopenclassrooms <maarijtarik@hotmail.com>
2024-03-26 05:52:51 +0100

    function declaration

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const myheader = document.querySelector(".myheader");
+/* const myheader = document.querySelector(".myheader");
 
 // myheader.remove()
 
@@ -11,4 +11,22 @@ div.prepend(title);
 
 title.classList.add("mb", "bc")
 title.classList.remove("bc")
-title.innerText = "Ali Hassan"
+title.innerText = "Ali Hassan" */
+
+const myheader = document.querySelector(".myheader");
+const section = document.getElementById("sec");
+const main = document.getElementById("main");
+const mudiv = document.getElementById("mudiv");
+
+const title = document.createElement("h1");
+
+function eldawlya (){
+        mudiv.append(title);
+
+        title.classList.add("mb");
+        
+        title.innerText = "الدولية للتجارة العالمية";
+}
+
+setTimeout( eldawlya , 4000)
+
