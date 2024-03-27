598b52731190d693b48197b08e6c3519c5a961fc
tarikopenclassrooms <maarijtarik@hotmail.com>
2024-03-27 04:28:08 +0100

    fonction expression

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// video 1 => 4
+
 /* const myheader = document.querySelector(".myheader");
 
 // myheader.remove()
@@ -13,7 +15,9 @@ title.classList.add("mb", "bc")
 title.classList.remove("bc")
 title.innerText = "Ali Hassan" */
 
-const myheader = document.querySelector(".myheader");
+
+// video 5 function declaration 
+/*const myheader = document.querySelector(".myheader");
 const section = document.getElementById("sec");
 const main = document.getElementById("main");
 const mudiv = document.getElementById("mudiv");
@@ -28,5 +32,66 @@ function eldawlya (){
         title.innerText = "الدولية للتجارة العالمية";
 }
 
-setTimeout( eldawlya , 4000)
+setTimeout( eldawlya , 4000)*/
+ 
+
+// video 6 fonction expression 
+/*const myheader = document.querySelector(".myheader");
+const section = document.getElementById("sec");
+const main = document.getElementById("main");
+const mudiv = document.getElementById("mudiv");
+
+const title = document.createElement("h1");*/
+
+/*function eldawlya() {
+  mudiv.append(title);
+
+  title.classList.add("mb");
+
+  title.innerText = "الدولية للتجارة العالمية";
+}
+
+setTimeout(eldawlya, 4000);
+console.log(typeof eldawlya);*/
+
+/* const container = function () {
+  mudiv.append(title);
+
+  title.classList.add("mb");
+
+  title.innerText = "الدولية للتجارة العالمية";
+};
+
+setTimeout( container , 6000)*/
+ 
+// video 6.1 arrow fonction 
+const myheader = document.querySelector(".myheader");
+const section = document.getElementById("sec");
+const main = document.getElementById("main");
+const mudiv = document.getElementById("mudiv");
+
+const title = document.createElement("h1");
+
+/*function eldawlya() {
+  mudiv.append(title);
+
+  title.classList.add("mb");
+
+  title.innerText = "الدولية للتجارة العالمية";
+}
+
+setTimeout(eldawlya, 4000);
+console.log(typeof eldawlya);*/
+
+const container = function () {
+  mudiv.append(title);
+
+  title.classList.add("mb");
+
+  title.innerText = "الدولية للتجارة العالمية";
+};
+
+setTimeout( container , 6000)
+
+// video 6.2 resumen
 
