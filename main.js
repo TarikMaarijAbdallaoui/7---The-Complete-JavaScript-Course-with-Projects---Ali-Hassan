e5797de74d5582593b5a4e869172f5cf96e5b8a4
tarikopenclassrooms <maarijtarik@hotmail.com>
2024-03-29 07:09:47 +0100

    add visa

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 
 const buy = document.getElementsByClassName("btn")[0]
 const biggerContainer = document.querySelector(".bigger-container")
+const buy1 = document.getElementsByClassName("btn")[1]
+const message = document.getElementById("message")
+const visa = document.getElementById("visa")
 
 buy.addEventListener("click", (eo) => { 
   biggerContainer.classList.add("active")
   eo.preventDefault()
+
+  message.style.display = "none"
 })
 
-const buy1 = document.getElementsByClassName("btn")[1]
 
 buy1.addEventListener("click", (eo) => { 
-  biggerContainer.classList.remove("active")
+  // biggerContainer.classList.remove("active")
+  message.style.display = "block"
+  visa.style.display = "none"
+
+  setTimeout(() => { biggerContainer.classList.remove("active")}, 3000)
  })
+
+ 
